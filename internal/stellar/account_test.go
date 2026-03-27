4baccada232f451ae55634130056a19e5df4ec99
vig-gray <victorygray59@gmail.com>
2026-03-27 01:43:54 +0100

    updated

--- internal/stellar/account_test.go
+++ internal/stellar/account_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/shopspring/decimal"
 	"github.com/stellar/go-stellar-sdk/clients/horizonclient"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // ============================================================================
